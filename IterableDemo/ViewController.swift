96680c3e2ef0579d24aea0f48a9f7d09b9827687
BJ Miller <bjmiller@mirror.co>
2022-05-02 14:53:10 -0400

    Add AppSpector e2e pod, comment out snapkit usage, build succeeds again

--- IterableDemo/ViewController.swift
+++ IterableDemo/ViewController.swift
@@ -5,15 +5,25 @@
 //  Created by BJ Miller on 4/29/22.
 //
 
+//import SnapKit
 import UIKit
 
 class ViewController: UIViewController {
 
+    let label = UILabel(frame: .zero)
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "foo"
+        view.addSubview(label)
+        NSLayoutConstraint.activate([
+            label.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            label.centerXAnchor.constraint(equalTo: view.centerXAnchor)
+        ])
+//        label.snp.makeConstraints { make in
+//            make.edges.equalToSuperview()
+//        }
     }
-
-
 }
-
